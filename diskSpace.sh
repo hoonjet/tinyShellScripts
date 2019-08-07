e1b93ed8669ad05cd07f84c16dec6727ad079330
Changhoon <30878360+hoonjet@users.noreply.github.com>
2019-08-07 12:05:12 +0900

    diskSpace.sh // check disk space by using df
    check disk space; if disk is almost full (defined in the code), automatically running autoremove

--- diskSpace.sh
+++ diskSpace.sh
@@ -6,7 +6,7 @@ used=$(df -h /boot | grep -vE '^Filesystem' | awk '{ print $5 }' | cut -d'%' -f1
 if [ $used -ge $ALERT ]; then
 
    echo "/boot is almost full: $used% running autoremove utility" 
-   sudo apt-get autoremove -y # Running autoremove utility (Ubuntu)
+   sudo apt-get autoremove -y # Running autoremove (for Ubuntu); comment this out if you don't want to run this utility
 
 else
 
